370552e0ff7772063cf1d36d4c998b0a66e5b888
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-06 13:46:40 -0600

    server is serving up static pages

--- server/server.js
+++ server/server.js
@@ -2,4 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
-var db = require()
+var db = require('./modules/db.js');
+
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+// add routes here
+
+app.listen(port, function(){
+  console.log('Listening on port:',port);
+});
